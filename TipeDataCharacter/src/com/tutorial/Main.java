e98a967e391e9edec96bac55f04fb2b226b5da0d
rio <riovectored@gmail.com>
2023-04-23 15:15:32 +0700

    menambahkan contoh code char

--- TipeDataCharacter/src/com/tutorial/Main.java
+++ TipeDataCharacter/src/com/tutorial/Main.java
@@ -3,8 +3,21 @@ package com.tutorial;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * Data Character (huruf) di Java direpresentasikan oleh tipe char.
+		 * Untuk membuat data char di Java, kita bisa menggunakn tanda ' (petik satu) diawal dan diakhir karakter
+		 * 
+		 * */
 
+		
+		
+		char r = 'R';
+		char i = 'I';
+		char o = 'O';
+		System.out.println(r);
+		System.out.println(i);
+		System.out.println(o);
+		
 	}
 
 }
