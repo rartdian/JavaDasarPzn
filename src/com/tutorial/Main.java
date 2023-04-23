c8c52f04ad1406c373c15205782ff526365c0bdc
rio <riovectored@gmail.com>
2023-04-23 14:26:19 +0700

    menambahkan tabel integer number pada tipe data number

---
+++ src/com/tutorial/Main.java
@@ -0,0 +1,20 @@
+package com.tutorial;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * Integer Number
+		 * -------------
+		 * tipe data | Min 						| Max 						| Size 	| Default
+		 * ----------------------------------------------------------------------------------
+		 * byte		 | -128						| 127 						|1 byte	| 0
+		 * short	 |-32,768					|32,767						|2 bytes| 0
+		 * int  	 |-2,147,483,648			|2,147,483,647				|4 bytes| 0
+		 * long		 |-9,223,372,036,854,775,808|9,223,372,036,854,775,807	|8 bytes| 0
+		 * 
+		 * */
+
+	}
+
+}
